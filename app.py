4552f3ea1cd9bcaf9143ac4a3657ba1070ed6421
akshita2k <akshita2k@gmail.com>
2021-05-21 23:12:08 +0530

    Second push

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import matplotlib
+""" import matplotlib
 matplotlib.use(‘TkAgg’)
 import matplotlib.pyplot as plt
 
@@ -6,4 +6,13 @@ import numpy as np
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()  
+plt.show()   """
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from datetime import datetime as dt
+
+app = dash.Dash(__name__)
+server = app.server
+app.layout = html.Div([item1, item2])
